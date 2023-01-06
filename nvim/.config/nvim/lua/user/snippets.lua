fc5817dd5f48e7b49e2ccf8ba1bf18e4567b344a
Michael Green <self@michaelgreen.dev>
2023-01-06 13:37:02 -0800

    dump

--- nvim/.config/nvim/lua/user/snippets.lua
+++ nvim/.config/nvim/lua/user/snippets.lua
@@ -1,1 +1,19 @@
-require("luasnip.loaders.from_vscode").lazy_load()
+
+local ls = require("luasnip")
+local vsc = require("luasnip.loaders.from_vscode")
+
+vsc.lazy_load()
+
+ls.add_snippets("python", {
+    ls.snippet(
+        "dump", {
+            ls.text_node({"if \""}),
+            ls.insert_node(1, "string"),
+            ls.text_node({"\" in __import__(\"json\").dumps("}),
+            ls.insert_node(2, "target"),
+            ls.text_node({"):", "    __import__(\"pdb\").set_trace()", "    "}),
+            ls.insert_node(0, "pass"),
+        }
+    )
+})
+
